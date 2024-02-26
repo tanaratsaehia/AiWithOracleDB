7289b7c5e3077838eb6efbac71ce6cf9c2498ea6
tananotorach <tanaratsaehia@gmail.cpm>
2024-02-26 20:19:25 +0700

    delete not use pages

--- Ui/pages/page_3.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-if st.button("Back"):
-    st.switch_page("pages/login.py")
--- Ui/pages/welcome.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(initial_sidebar_state="collapsed", page_title="welcome")
-
-
